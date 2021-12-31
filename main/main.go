dcd54875524bb3893a58ed34075422f559e26892
Andrew Dykema <atdykema@gmail.com>
2021-12-31 13:18:06 -0500

    cleared house, set up CLI interaction tool

--- main/main.go
+++ main/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
-	"fmt" 
-	"github.com/atdykema/blockchain_golang/genesisTools"
-	/* "net" */
-	/* "os" */
-	"github.com/atdykema/blockchain_golang/connection"
-	
+	"fmt"
+	"time"
 )
 
 func main(){
-	
 
-	go conn.InitListen()
+	for {
 
+		fmt.Print("[",time.Now(),"]: ")
+
+		var currCmd string
+		fmt.Scanln(&currCmd)
+
+		if handleCmd(currCmd) == -1{
+			break
+		}
+
+	}
+
+
+}
+
+func handleCmd(command string) int {
+
+	fmt.Print("[",time.Now(),"]: ")
+
+	switch command{
+
+	case "hello":
+		fmt.Println("hi there")
+		return 0
+
+	case "exit":
+		fmt.Println("Exiting...")
+		return -1
+		
+	default:
+		fmt.Println("Unrecognized command")
+		return 0
+	}
 
 
 }
