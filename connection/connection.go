377e830618193206a28d6dfad0e89401131d743a
Andrew Dykema <atdykema@gmail.com>
2022-01-01 23:30:48 -0500

    tcp server working

---
+++ connection/connection.go
@@ -0,0 +1,56 @@
+package conn
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+)
+
+func StartServer(address string, port string){
+
+	listener, err := net.Listen("tcp", (address + ":" + port))
+	if err != nil{
+		fmt.Println("error listening: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Currently listening on " + address + ":" + port  + "...")
+
+	for{
+
+		connection, err := listener.Accept()
+		if err != nil{
+			fmt.Println("error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(connection)
+
+	}
+
+}
+
+
+func handleRequest(connection net.Conn){
+
+	buf := make([]byte, 1024)
+
+	reqLen, err := connection.Read(buf)
+	if err != nil {
+		fmt.Println("error reading: ", err.Error(), " ", reqLen)
+		os.Exit(1)
+	}
+
+	connection.Write([]byte("returning message"))
+
+	connection.Close()
+}
+
+func FindPeers() {
+
+	
+}
+
